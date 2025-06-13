2d0e433522c573ef06e8cd99737c7d88ea22e9f2
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2025-06-13 17:57:26 -0400

    Lower wildcard pattern

--- compiler/crates/lower/src/patterns/wildcard.rs
+++ compiler/crates/lower/src/patterns/wildcard.rs
@@ -1,13 +1,27 @@
 use crate::{Visit, Visitor};
 use wipple_compiler_syntax::WildcardPattern;
-use wipple_compiler_trace::{rule, NodeId, Rule};
+use wipple_compiler_trace::{NodeId, Rule, rule};
+use wipple_compiler_typecheck::nodes::ConstraintNode;
 
 rule! {
-    // TODO
+    /// A wildcard pattern.
+    wildcard_pattern: Typed;
 }
 
 impl Visit for WildcardPattern {
-    fn visit<'a>(&'a self, visitor: &mut Visitor<'a>, parent: Option<(NodeId, impl Rule)>) -> NodeId {
-        todo!()
+    fn visit<'a>(
+        &'a self,
+        visitor: &mut Visitor<'a>,
+        parent: Option<(NodeId, impl Rule)>,
+    ) -> NodeId {
+        visitor.node(parent, &self.range, |visitor, _id| {
+            (
+                ConstraintNode {
+                    value: visitor.parent(),
+                    constraints: Vec::new(),
+                },
+                rule::wildcard_pattern,
+            )
+        })
     }
 }
