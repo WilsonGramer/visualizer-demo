a6c016d53c5561df383ffdc28a7bedb90ee1d9aa
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2025-06-16 20:12:00 -0400

    Fix Vite allowlist

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,6 +1,16 @@
-import { defineConfig } from "vite";
+import process from "node:process";
+import path from "node:path";
+import { defineConfig, searchForWorkspaceRoot } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [tailwindcss()],
+    server: {
+        fs: {
+            allow: [
+                searchForWorkspaceRoot(process.cwd()),
+                path.resolve(__dirname, "../compiler/pkg"),
+            ],
+        },
+    },
 });
